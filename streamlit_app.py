3120cf2f2996baacaf84afbd1000eb1fdeebf08b
AlDings <firdausalif60@gmail.com>
2024-06-28 11:34:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,11 @@ def main() :
   st.dataframe(house)
 
   st.write('Metrics')
-  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+  st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
+
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  AgGrid(titanic)
+  
+  st.table([x for x in range(1,5)])
 if __name__ == '__main__' : 
   main()
